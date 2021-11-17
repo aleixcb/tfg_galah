ff13959f315cafefd991139325cd744561a669ba
Aleix <aleixcb99@gmail.com>
2021-11-17 21:22:15 +0100

    canvis 11/11/21 + comentaris

--- py/obtain_data.py
+++ py/obtain_data.py
@@ -5,27 +5,33 @@ GALAH EDR3 RC data for tSNE
 """
 
 import numpy as np
-from astropy.io import fits as pyfits
-from astropy.table import Table
-import scipy
+from astropy.io import fits as pyfits   #acces als files .fits (Flexible Image Transport System)
+from astropy.table import Table   #manipular taules
+import scipy    #biblioteca per mates
 
-def sqadd(a, b):
+def sqadd(a, b):   #sqrt(a^2 + b^2)
     "Add 2 values in quadrature"
     return np.sqrt(a*a + b*b)
 
-class galah_dr3_rc(object):
-    def __init__(self, abundances=True):
+class galah_dr3_rc(object):       #agrupem les funcions dins la classe galah_dr3_rc
+    def __init__(self, abundances=True): 
         """
-        Open the GALAH DR3 red clump catalogue and applies some cuts on the 
+        Open the GALAH DR3 red clump catalogue (and applies some cuts on the 
         flags.
         
         Based on the flag statistics in the catalogue:
-        * ../data/GALAH_DR3_joint_allstar_v2_redclump_flagstatistics.txt
-        we exclude the elements Li, C,Rb, Sr, Mo, Ru, Sm
+        * ../data/GALAH_DR3_joint_allstar_v2_redclump_flagstatistics.txt  (descarregat)
+        we exclude the elements Li, C, Rb, Sr, Mo, Ru, Sm
+        
+        
+        
+        La funció obre el catàleg de GALAH DR3 RC i aplica condicions a les flags (que siguin bones mesures).
+        Excloem els elements Li, C,Rb, Sr, Mo, Ru, Sm per ser males mesures
+        
         """
-        hdu = pyfits.open('../data/GALAH_DR3_joint_allstar_v2_redclump.fits', names=True)
-        data = hdu[1].data
-        if abundances:
+        hdu = pyfits.open('../data/GALAH_DR3_joint_allstar_v2_redclump.fits', names=True) #HDU (Header Data Unit)
+        data = hdu[1].data  #extensió 1
+        if abundances:  # (=True)
             data = data[ (data['flag_sp']==0) * (data['flag_fe_h']==0) * 
                          (data['flag_o_fe']==0) * (data['flag_na_fe']==0) * 
                          (data['flag_mg_fe']==0) * (data['flag_al_fe']==0) * 
@@ -43,38 +49,45 @@ class galah_dr3_rc(object):
         self.data = data
         return None
 
-    def get_ndimspace(self, age=False, kin=False, feh=True, norm="stdev"):
+    def get_ndimspace(self, feh=True, norm="stdev"):  #espai ndimensional d'abundàncies
         """
         We select the dimensions of the GALAH DR3 chemical abundance space:
         * Fe, O, Na, Mg, Al, Si, K, Ca, Sc, Ti, V, Cr, Mn, Co, Ni, Cu, Zn, Y, Zr, Ba, La, Ce, Nd, Eu
+        (24 elements)
+        
         """
         data = self.data
-        X        = np.c_[data['fe_h'],data['o_fe'],data['na_fe'],data['mg_fe'],
+        
+        #MATRIU ABUNDANCIES (dim = 37417 estrelles x 24 elements = files x columnes)
+""" np.c_ concatena matrius en columns (cap a la dreta), així com np.r_ concatena matrius en files(cap avall) """
+        X        = np.c_[data['fe_h'],data['o_fe'],data['na_fe'],data['mg_fe'],  
                          data['al_fe'],data['si_fe'],data['k_fe'],data['ca_fe'],
                          data['sc_fe'],data['ti_fe'],data['v_fe'],data['cr_fe'],
                          data['mn_fe'],data['co_fe'],data['ni_fe'],data['cu_fe'],
                          data['zn_fe'],data['y_fe'],data['zr_fe'],data['ba_fe'],
-                         data['la_fe'],data['ce_fe'],data['nd_fe'],data['eu_fe'],
-                         data['age_bstep']]
+                         data['la_fe'],data['ce_fe'],data['nd_fe'],data['eu_fe'] ]
+        
+        
+        #MATRIU ERRORS
         Xerr1    = np.c_[data['e_fe_h'],data['e_o_fe'],data['e_na_fe'],data['e_mg_fe'],
                          data['e_al_fe'],data['e_si_fe'],data['e_k_fe'],data['e_ca_fe'],
                          data['e_sc_fe'],data['e_ti_fe'],data['e_v_fe'],data['e_cr_fe'],
                          data['e_mn_fe'],data['e_co_fe'],data['e_ni_fe'],data['e_cu_fe'],
                          data['e_zn_fe'],data['e_y_fe'],data['e_zr_fe'],data['e_ba_fe'],
-                         data['e_la_fe'],data['e_ce_fe'],data['e_nd_fe'],data['e_eu_fe'],
-                         data['e_age_bstep']]
+                         data['e_la_fe'],data['e_ce_fe'],data['e_nd_fe'],data['e_eu_fe'] ]
 
         # Take care of the 0.0 uncertainties: forced minimum to 0.03 
         Xerr1[:, :] = np.maximum(Xerr1, 0.03*np.ones(Xerr1.shape))
-        Xerr     = np.mean( Xerr1, axis=0)
+        """ compara la matriu d'errors amb una matriu plena de 0,03's element a element, per agafar el valor més
+        gran dels dos (força un mínim error de 0,03) """
+        Xerr     = np.mean( Xerr1, axis=0) #dim=1x24 # fa la mitjana dels errors de l'abundància d'un element per 
+#                                                      totes les estrelles
+        """ calcula la mitjana dels elements de la matriu seguint un dels eixos
+        (axis=0 seguint la columna i axis=1 seguint la fila) """
+        
 
-        if not age:
-            X = X[:,:-1]; Xerr = Xerr[:-1]
-        if kin:
-            X    = np.append(X, np.vstack((data['vR_Rzphi'],data['vT_Rzphi'],data['vZ_Rzphi'])).T, axis=1)
-            Xerr = np.append(Xerr, np.array([10,10,10]).T, axis=0)
         if not feh:
-            X = X[:,1:]; Xerr = Xerr[1:]
+            X = X[:,1:]; Xerr = Xerr[1:]  #exclou [Fe/H] de les matrius (1a columna de X i 1r element de Xerr)
 
         if norm == "hogg2016":
             # Normalise everything by the typical range as in Hogg+2016: 
@@ -92,17 +105,17 @@ class galah_dr3_rc(object):
 
     def get_ndimspace_H(self, cn=True, age=False, norm="stdev"):
         """
-        Cut out missing data and prepare t-SNE input array
+        Cut out missing data and prepare t-SNE input array  (separem les dades que faltin)
         Optional:
             age: Bool  - include age in the analysis
             cn:  Bool  - include [C/H] & [N/H], default: True
             norm: str  - normalisation method, default: "stdev"
         """
-        # For giants, everything up to Cu is okay
+        # For giants, everything up to Cu is okay (agafem fins al Cu)
         X        = self.data['X_H'][:, :20]
         Xerr     = np.mean(self.data['X_H_ERR'][:, :20], axis=0)
         if not cn:
-            X = X[:,2:]; Xerr = Xerr[2:]
+            X = X[:,2:]; Xerr = Xerr[2:]  #excloem [C/H] & [N/H]
         if norm == "hogg2016":
             # Normalise everything by the typical range as in Hogg+2016: 
             Xnorm    = (X/Xerr[np.newaxis,:])
